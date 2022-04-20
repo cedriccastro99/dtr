5a2560ccdd7d585729021477767ea97ff8d5af9d
cedriccastro99 <cedriccastro150@gmail.com>
2022-04-20 23:04:16 +0800

    -

--- views/register.php
+++ views/register.php
@@ -48,7 +48,7 @@
         </div>
         <div class="mb-3">
             <label for="agency" class="form-label">Agency</label>
-            <input name="agency" id="agency" type="agency" class="form-control" placeholder="Enter agency">
+            <input name="agency" id="agency" type="agency" class="form-control" placeholder="Enter username">
         </div>
         <span id="alert_box">
         </span>
